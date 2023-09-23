2eaac5cdb3bfa5366cc9e9a9c8781e3673ac533a
ManoharaDraksharam <dmranganath@gmail.com>
2023-09-23 15:01:48 +0530

    Python files

---
+++ Recognition_Face5.py
@@ -0,0 +1,84 @@
+import face_recognition
+import cv2
+import numpy as np
+import os
+
+known_faces_dir = "Known_faces"
+
+
+print("loading known faces")
+
+known_faces_encoding = []
+known_names = []
+
+
+for name in os.listdir(known_faces_dir):
+	for filename in os.listdir(f"{known_faces_dir}/{name}"):
+	
+		image = face_recognition.load_image_file(f"{known_faces_dir}/{name}/{filename}")
+		face_encoding = face_recognition.face_encodings(image)[0]
+		known_faces_encoding.append(face_encoding)
+		known_names.append(name)
+
+
+video_capture = cv2.VideoCapture(0)
+
+print("processing unknown faces")
+
+unknown_face_location1 = []
+unknown_face_encoding1 = []
+
+face_names = []
+
+
+while True:
+   
+	ret, frame = video_capture.read()
+
+	small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)     
+	RGB = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+          
+	unknown_face_location1 = face_recognition.face_locations(RGB)
+	unknown_face_encoding1 = face_recognition.face_encodings(RGB,unknown_face_location1)
+	        
+	for face_encoding in unknown_face_encoding1:
+	
+		matches = face_recognition.compare_faces(known_faces_encoding, face_encoding)
+		name1 = "Unknown"
+            	
+		face_distances = face_recognition.face_distance(known_faces_encoding, face_encoding)
+		
+		best_match_index = np.argmin(face_distances)
+                                                
+		if matches[best_match_index]:
+            		name1 = known_names[best_match_index]
+
+		face_names.append(name1)
+
+       
+	for (top, right, bottom, left), name2 in zip(unknown_face_location1, face_names):
+        
+        	top *= 4
+        	right *= 4
+        	bottom *= 4
+        	left *= 4
+
+        
+        	cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        
+        	cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        	font = cv2.FONT_HERSHEY_DUPLEX
+        	cv2.putText(frame, name2, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+
+	cv2.imshow('Video', frame)
+
+    
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+        	break
+
+video_capture.release()
+cv2.destroyAllWindows()
+
+
+
---
+++ opcv1.py
@@ -0,0 +1,49 @@
+import numpy as np
+import cv2 as cv
+import os, fnmatch
+
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+	print("Cannot open camera")
+	exit()
+	
+i=0
+
+while True:
+# Capture frame-by-frame
+	ret, frame = cap.read()
+	
+# if frame is read correctly ret is True
+	if not ret:
+		print("Can't receive frame (stream end?). Exiting ...")
+		break
+	
+# Our operations on the frame come here
+	gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+ 
+# Display the resulting frame
+	cv.imshow('frame', gray)
+	
+	'''image_files = find('*.png', '/home/manohara/PythonProjects')
+	
+	print(image_files)'''
+	
+	a=cv.waitKey(1) 
+	
+	'print(a)
+	
+	if a == ord('c'):
+		i = i+1
+		cv.imwrite("captured"+str(i)+".png", frame)
+	elif a == ord('q'):
+		break
+	
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
+  
+
+
+
+
